189010a9e6dd378952eb68868ca7940c58bb58a2
Leo Fourniols <leofourniols@gmail.com>
2021-02-12 14:22:18 +0100

    modif produit branch perso

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -3,16 +3,18 @@ package firstProjectJAVA;
 public class Produit {
 	public Long idProduit;
 	public int code;
+	public int nbr;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, int code) {
+	public Produit(Long idProduit, int code, int nbr) {
 		this.idProduit = idProduit;
 		this.code = code;
+		this.nbr = nbr;
 	}
 	@Override
 	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code +"]";
+		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code + ", nombreProduits= " + nbr + "]";
 	}
 	
 	
--- src/firstProjectJAVA/Test.java
+++ src/firstProjectJAVA/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Utilisateur user1 = new Utilisateur(1l, "Ayari", "Oussama","a","a");
 		System.out.println(user1.toString());
-		Produit product1 = new Produit(1l, "Lait", 123);
+		Produit product1 = new Produit(1l, 123, 12);
 		System.out.println(product1.toString());
 	}
 
