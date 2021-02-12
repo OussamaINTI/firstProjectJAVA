98dc6bdf88e25fd6a4f883813bf8a3ac53c95cab
KarineAverseng <karine.averseng@laposte.net>
2021-02-12 14:28:35 +0100

    utilisateur

--- src/firstProjectJAVA/Utilisateur.java
+++ src/firstProjectJAVA/Utilisateur.java
@@ -3,7 +3,6 @@ package firstProjectJAVA;
 public class Utilisateur {
 	public Long idUtilisateur;
 	public String nomUtilisateur;
-<<<<<<< HEAD
 	public String prenomUtilisateur;
 	public String usernameUtilisateur;
 	public String passwordUtilisateur;
@@ -26,35 +25,15 @@ public class Utilisateur {
 		this.prenomUtilisateur = prenomUtilisateur;
 		this.usernameUtilisateur = usernameUtilisateur;
 		this.passwordUtilisateur = passwordUtilisateur;
-=======
-	public String prenomUtilisateur,username,password;
-
-	public Utilisateur() {
-	}
-
-	public Utilisateur(Long idUtilisateur, String nomUtilisateur, String prenomUtilisateur, String password,String username) {
-		this.idUtilisateur = idUtilisateur;
-		this.nomUtilisateur = nomUtilisateur;
-		this.prenomUtilisateur = prenomUtilisateur;
-		this.password = password;
-		this.username=username;
->>>>>>> 30d2a229cb359d7c3afc393f95bc40e857f5715a
 	}
 
 	@Override
 	public String toString() {
 		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nomUtilisateur=" + nomUtilisateur
-<<<<<<< HEAD
+
 				+ ", prenomUtilisateur=" + prenomUtilisateur + ", usernameUtilisateur=" + usernameUtilisateur + ", passwordUtilisateur=" + passwordUtilisateur
 				+ "]";
 	}
-=======
-				+ ", prenomUtilisateur=" + prenomUtilisateur + ", username=" + username + ", password=" + password
-				+ "]";
-	}
 
-
-
->>>>>>> 30d2a229cb359d7c3afc393f95bc40e857f5715a
 
 }
