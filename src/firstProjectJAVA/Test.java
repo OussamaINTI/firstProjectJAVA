95070fc488bd6bf4c2098070bf00da515fb915ce
Leo Fourniols <leofourniols@gmail.com>
2021-02-12 11:47:59 +0100

    ajout code dans produit et test

--- src/firstProjectJAVA/Test.java
+++ src/firstProjectJAVA/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Utilisateur user1 = new Utilisateur(1l, "Ayari", "Oussama");
 		System.out.println(user1.toString());
-		Produit product1 = new Produit(1l, "Lait");
+		Produit product1 = new Produit(1l, "Lait", 123);
 		System.out.println(product1.toString());
 	}
 
