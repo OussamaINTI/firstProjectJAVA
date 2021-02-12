b1313fee86f29bac7ebe2e6d30d82127ed1378c2
mathieuportal <mathieuportal06@gmail.com>
2021-02-12 14:22:57 +0100

    Added nbrProduit & nbrStock

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -4,17 +4,21 @@ public class Produit {
 	public Long idProduit;
 	public String nomProduit;
 	public int code;
+	public int nbrProduit;
+	public int nbrStock;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, String nomProduit, int code) {
+	public Produit(Long idProduit, String nomProduit, int code, int nbrProduit, int nbrStock) {
 		this.idProduit = idProduit;
 		this.nomProduit = nomProduit;
 		this.code = code;
+		this.nbrProduit = nbrProduit;
+		this.nbrStock = nbrStock;
 	}
 	@Override
 	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", codeProduit=" + code + "]";
+		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", codeProduit=" + code + ", nbrProduit=" + nbrProduit + ", nbrStock=" + nbrStock + "]";
 	}
 	
 	
