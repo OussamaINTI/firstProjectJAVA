af147a00500a9e35e49572b35d3890dd9cf6675c
ouss123456 <oussama.adaming@gmail.com>
2021-02-12 14:08:31 +0100

    commit dans la branche local

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -3,16 +3,18 @@ package firstProjectJAVA;
 public class Produit {
 	public Long idProduit;
 	public int code;
+	public String img;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, int code) {
+	public Produit(Long idProduit, int code, String img) {
 		this.idProduit = idProduit;
 		this.code = code;
+		this.img = img;
 	}
 	@Override
 	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code + "]";
+		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code + "image="+img+"]";
 	}
 	
 	
