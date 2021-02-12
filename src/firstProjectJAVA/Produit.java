b033d0252e6ff980a56a7c902aa5e2bd6e3e25b5
ouss123456 <oussama.adaming@gmail.com>
2021-02-12 14:10:48 +0100

    commit dans la branche correctif

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -3,18 +3,16 @@ package firstProjectJAVA;
 public class Produit {
 	public Long idProduit;
 	public int code;
-	public String img;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, int code, String img) {
+	public Produit(Long idProduit, int code) {
 		this.idProduit = idProduit;
 		this.code = code;
-		this.img = img;
 	}
 	@Override
 	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code + "image="+img+"]";
+		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code +"]";
 	}
 	
 	
