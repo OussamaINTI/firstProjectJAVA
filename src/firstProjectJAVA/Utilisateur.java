dc33820d8703c2fa7ef5aeabc7f9c019b69c4143
KarineAverseng <karine.averseng@laposte.net>
2021-02-12 12:35:01 +0100

    modification utilisateur

--- src/firstProjectJAVA/Utilisateur.java
+++ src/firstProjectJAVA/Utilisateur.java
@@ -4,21 +4,34 @@ public class Utilisateur {
 	public Long idUtilisateur;
 	public String nomUtilisateur;
 	public String prenomUtilisateur;
+	public String username;
+	public String password;
 
 	public Utilisateur() {
 	}
-
+	
 	public Utilisateur(Long idUtilisateur, String nomUtilisateur, String prenomUtilisateur) {
+		super();
 		this.idUtilisateur = idUtilisateur;
 		this.nomUtilisateur = nomUtilisateur;
 		this.prenomUtilisateur = prenomUtilisateur;
 	}
 
+	public Utilisateur(Long idUtilisateur, String nomUtilisateur, String prenomUtilisateur, String username,
+			String password) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.nomUtilisateur = nomUtilisateur;
+		this.prenomUtilisateur = prenomUtilisateur;
+		this.username = username;
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nomUtilisateur=" + nomUtilisateur
-				+ ", prenomUtilisateur=" + prenomUtilisateur + "]";
+				+ ", prenomUtilisateur=" + prenomUtilisateur + ", username=" + username + ", password=" + password
+				+ "]";
 	}
-	
 
 }
