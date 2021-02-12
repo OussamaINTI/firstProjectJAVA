4e1da28bbdff519dc54276e6174941268caa4db4
atomystic <gnavo.tony@hotmail.fr>
2021-02-12 14:21:46 +0100

    [ADD] ajout de deux paramètres

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -4,13 +4,16 @@ public class Produit {
 	public Long idProduit;
 	public String nomProduit;
 	public int code;
+	public int nbreProduit,nbrStock;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, String nomProduit, int code) {
+	public Produit(Long idProduit, String nomProduit, int code,int nbrProduit , int nbrStock) {
 		this.idProduit = idProduit;
 		this.nomProduit = nomProduit;
 		this.code = code;
+		this.nbreProduit = nbrProduit;
+		this.nbrStock = nbrStock;
 	}
 	@Override
 	public String toString() {
