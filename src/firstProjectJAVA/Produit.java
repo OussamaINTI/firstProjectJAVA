a43efbcb77fc209a1860e8d5e6035b0d8621fd1e
youssefoubenammou <youssef3396@gmail.com>
2021-02-12 14:28:59 +0100

    ajout attribut produit

--- src/firstProjectJAVA/Produit.java
+++ src/firstProjectJAVA/Produit.java
@@ -3,13 +3,18 @@ package firstProjectJAVA;
 public class Produit {
 	public Long idProduit;
 	public int code;
+	public int nbProduit;
+	public boolean bio;
 	
 	public Produit() {
 	}
-	public Produit(Long idProduit, int code) {
+	public Produit(Long idProduit, int code,int nbProduit,boolean bio) {
 		this.idProduit = idProduit;
 		this.code = code;
+		this.nbProduit = nbProduit;
+		this.bio = bio;
 	}
+
 	@Override
 	public String toString() {
 		return "Produit [idProduit=" + idProduit + ", codeProduit=" + code +"]";
